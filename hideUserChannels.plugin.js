4ca52ee6f072bc9207cf18786f9494b2f372d13f
secuestrador <116821576+secuestrador@users.noreply.github.com>
2023-12-26 19:32:16 -0300

    Update hideUserChannels.plugin.js
    Fixed

--- hideUserChannels.plugin.js
+++ hideUserChannels.plugin.js
@@ -6,33 +6,37 @@
  */
 
 module.exports = class YourPlugin {
-    start() {
-      var namesToDetect = [`Username1`, `Username2`, `Username3`];
+    start() {// Definir la función que contiene tu código
+function executeCode() {
+    var namesToDetect = [`Username1`, `Username2`, `Username3`];
+    var elements = document.getElementsByClassName('usernameFont__71dd5');
 
-      var elements = document.getElementsByClassName('usernameFont__71dd5');
-  
-      for (var i = 0; i < elements.length; i++) {
-          var currentElement = elements[i];
-          var username = currentElement.textContent.trim();
-  
-          if (namesToDetect.includes(username)) {
-              var fifthParent = getNthParent(currentElement, 5);
-              if (fifthParent) {
-                  fifthParent.style.display = 'none';
-              }
-          }
-      }
-  
-      function getNthParent(element, n) {
-          for (var i = 0; i < n; i++) {
-              if (element.parentNode) {
-                  element = element.parentNode;
-              } else {
-                  return null;
-              }
-          }
-          return element;
-      }
+    for (var i = 0; i < elements.length; i++) {
+        var currentElement = elements[i];
+        var username = currentElement.textContent.trim();
+
+        if (namesToDetect.includes(username)) {
+            var fifthParent = getNthParent(currentElement, 5);
+            if (fifthParent) {
+                fifthParent.style.display = 'none';
+            }
+        }
+    }
+
+    function getNthParent(element, n) {
+        for (var i = 0; i < n; i++) {
+            if (element.parentNode) {
+                element = element.parentNode;
+            } else {
+                return null;
+            }
+        }
+        return element;
+    }
+}
+
+setInterval(executeCode, 100);
+
     } 
 
     stop() {
